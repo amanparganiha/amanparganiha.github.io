e9dfd9bf395e8b488836a06949f6d808db6d055f
Aman Parganiha <72440381+amanparganiha@users.noreply.github.com>
2026-03-02 19:00:08 +0530

    Update Formspree endpoint in Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -27,7 +27,7 @@ const Contact = () => {
   if (!validate()) return;
 
   try {
-    const response = await fetch("https://formspree.io/f/mqedlqar", {
+    const response = await fetch("https://formspree.io/f/xnjbknpg", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
